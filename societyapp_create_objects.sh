93c96dd70824a3d0db11a29c12ed087e22df12c8
Balaji Londhe <blondhe1@in.ibm.com>
2019-12-12 19:06:01 +0530

    Fixes

--- societyapp_create_objects.sh
+++ societyapp_create_objects.sh
@@ -4,7 +4,10 @@ oc new-project societyapp-dev
 oc new-app sonatype/nexus
 oc expose svc/nexus
 sleep 10
+oc new-app --docker-image=openshiftdemos/sonarqube:6.5
+oc expose svc/sonarqube
+sleep 10
 oc new-app -f societyapp-dev.yaml
-echo "Creating deployment objects and postegre db instance for society-test project"
-oc new-project societyapp-test
-oc new-app -f societyapp-test.yaml
+#echo "Creating deployment objects and postegre db instance for society-test project"
+#oc new-project societyapp-test
+#oc new-app -f societyapp-test.yaml
